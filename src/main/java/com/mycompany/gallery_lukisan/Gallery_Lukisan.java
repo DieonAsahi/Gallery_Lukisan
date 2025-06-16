a4c3d4d910cad2e84dd02d5da259638914c293b2
DieonAsahi <athawaguth@gmail.com>
2025-06-16 09:21:58 +0700

    UAS

---
+++ src/main/java/com/mycompany/gallery_lukisan/Gallery_Lukisan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gallery_lukisan;
+
+/**
+ *
+ * @author athaw
+ */
+public class Gallery_Lukisan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
